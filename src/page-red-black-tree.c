ffe9af8ed7c0ca3df5e2aab05900446da5b68ffc
KPMGE <jesuskevin254@gmail.com>
2023-01-28 17:17:48 -0300

    :recycle: update page-red-black-tree

--- src/page-red-black-tree.c
+++ src/page-red-black-tree.c
@@ -7,25 +7,19 @@
 #include <stdlib.h>
 #include <math.h>
 
-// the max number of incoming_links to a especific node
-#define MAX_INCOMING_LINKS 1000
-
 #define DAMPING_FACTOR 0.85
 #define EPSILON 10e-10
 
 // NOTE: i'm using those global variables in here, cuz currently, i needa use the 'grbt_search' function to 
 // get a node given its name, but as soon as a linked-list is implemented, there is no need for that anymore!
 GRBT *rbt_static = NULL;
-// i need the total amount of pages too, probably there is a better way to store that value, but it depends on 
-// the approach used to refactor the code
-int total_pages_static = 0;
 
 typedef struct {
   double page_rank; 
   int num_incoming_links;
-  //char *incoming_links[MAX_INCOMING_LINKS];
   LinkedList *incoming_links;
   int num_outgoing_links;
+  int total_pages;
 } PageData;
 
 static void *alloc_initial_page_data() {
@@ -38,21 +32,6 @@ static void *alloc_initial_page_data() {
 
   return d;
 }
-
-// static void print_data(void *data) {
-//   PageData *d = data;
-//   printf("\nnum_outgoing_links: %d", d->num_outgoing_links);
-//   printf("\nnum_incoming_links: %d\n", d->num_incoming_links);
-
-//   printf("incoming_links: ");
-//   for (int i = 0; i < d->num_incoming_links; i++) {
-//     if (i % 7 == 0) {
-//       printf("\n");
-//     }
-//     printf("%s ", d->incoming_links[i]);
-//   }
-//   printf("\n");
-// }
 
 static void free_data(void *data) {
   PageData *d = data;
@@ -81,11 +60,8 @@ void page_rbt_insert(PRBT *h, char *page_name) {
   GRBT *node = grbt_search(h->rbt, page_name);
   PageData *data = grbt_data(node);
   data->page_rank = 1.0 / h->total_pages;
+  data->total_pages = h->total_pages;
 }
-
-// void page_rbt_print(PRBT *h) {
-//   grbt_print(h->rbt, print_data);
-// }
 
 void page_rbt_add_links(PRBT *h, char *page_name, int num_outgoing_links, char **links) {
   // find page node in the generic rbt
@@ -101,7 +77,6 @@ void page_rbt_add_links(PRBT *h, char *page_name, int num_outgoing_links, char *
 
   // insert the links in the rbt, just in case
   for (int i = 0; i < num_outgoing_links; i++) {
-    // printf("inserting '%s' on the tree\n", links[i]);
     page_rbt_insert(h, links[i]);
   }
 
@@ -110,9 +85,6 @@ void page_rbt_add_links(PRBT *h, char *page_name, int num_outgoing_links, char *
     GRBT *node = grbt_search(h->rbt, links[i]);
     PageData *data = grbt_data(node);
     int p = data->num_incoming_links++;
-    //if (p > MAX_INCOMING_LINKS) exit(1);
-    // printf("store %s in the incoming_links array of %s\n", page_name, links[i]);
-    //data->incoming_links[p] = strdup(page_name);
     linked_list_insert(data->incoming_links, page_name);
   }
 }
@@ -129,7 +101,7 @@ static void update_page_ranks(void *data) {
   if (!data) return;
 
   // initilize constant for initial new page-rank
-  double new_page_rank = (1.0 - DAMPING_FACTOR) / total_pages_static;
+  double new_page_rank = (1.0 - DAMPING_FACTOR) / page->total_pages;
 
   // if |Out(i)| == 0
   if (page->num_outgoing_links == 0) {
@@ -161,8 +133,6 @@ static double get_page_rank(GRBT *h, char *page_name) {
 double calculate_page_rank(PRBT *p, char *page_name, int total_pages) {
   // update the global variable with the rbt from the page-rbt. this is awful and ought be refactored!!
   rbt_static = p->rbt;
-  // update the global variable with the total_pages. this is awful and ought be refactored!!
-  total_pages_static = total_pages;
 
   // error starts as infinity
   double error = INFINITY;
@@ -170,15 +140,12 @@ double calculate_page_rank(PRBT *p, char *page_name, int total_pages) {
 
   while (error > EPSILON) {
     // get the page-rank of the page before updating.
-    // NOTE: as i said, as currently the implementation's been made with a char **, i needa 
-    // keep searching the rbt to get the nodes, which is bad
     double prev_rank = get_page_rank(p->rbt, page_name);
 
+    // traverse rbt, updating the links
     traverse_tree(p->rbt, update_page_ranks);
 
     // get the page-rank of the page after updating
-    // NOTE: as i said, as currently the implementation's been made with a char **, i needa 
-    // keep searching the rbt to get the nodes, which is bad
     new_rank = get_page_rank(p->rbt, page_name);
 
     // update error
